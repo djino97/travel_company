03f5747f2b748466e7426369d8d6834055c4d242
djino97 <38541098+djino97@users.noreply.github.com>
2019-05-11 13:54:53 +0300

    Update settings.py

--- kursachback/kursachback/settings.py
+++ kursachback/kursachback/settings.py
@@ -81,8 +81,8 @@ WSGI_APPLICATION = 'kursachback.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
-DATABASES['default']['ENGINE'] = 'django.db.backends.mysql'
 DATABASES['default'] = dj_database_url.config(default = 'mysql://bb04d450dd01d7:1c53481c@eu-cdbr-west-02.cleardb.net/heroku_a6fca33c3cc6106')
+DATABASES['default']['ENGINE'] = 'django.db.backends.mysql'
 GDAL_LIBRARY_PATH = os.getenv('GDAL_LIBRARY_PATH')
 GEOS_LIBRARY_PATH = os.getenv('GEOS_LIBRARY_PATH')
 
