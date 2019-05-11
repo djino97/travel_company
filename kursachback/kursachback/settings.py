b9cf48e1674b24700d664420e81f0d8f3cacff82
djino97 <38541098+djino97@users.noreply.github.com>
2019-05-11 13:49:22 +0300

    Update settings.py

--- kursachback/kursachback/settings.py
+++ kursachback/kursachback/settings.py
@@ -81,17 +81,16 @@ WSGI_APPLICATION = 'kursachback.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
-DATABASES['default'] = dj_database_url.config(default = 'mysql://bb04d450dd01d7:1c53481c@eu-cdbr-west-02.cleardb.net/heroku_a6fca33c3cc6106?reconnect=true')
+DATABASES['default']['ENGINE'] = 'django.db.backends.mysql'
+DATABASES['default'] = dj_database_url.config(default = 'mysql://bb04d450dd01d7:1c53481c@eu-cdbr-west-02.cleardb.net/heroku_a6fca33c3cc6106')
 GDAL_LIBRARY_PATH = os.getenv('GDAL_LIBRARY_PATH')
 GEOS_LIBRARY_PATH = os.getenv('GEOS_LIBRARY_PATH')
 
-DATABASES = {
-    'default': {
-
-#DATABASES['default']['ENGINE'] = 'django.db.backends.mysql'
-    
-    'NAME': 'heroku_a6fca33c3cc6106',
-    'ENGINE': 'django.db.backends.mysql',
+#DATABASES = {
+    #'default': {
+    #'ENGINE': 'django.db.backends.mysql',
+    #'NAME': 'heroku_a6fca33c3cc6106',
+   # 'ENGINE': 'django.db.backends.mysql',
         #'USER': 'bb04d450dd01d7',
         #'PASSWORD': '1c53481c',
         #'HOST': 'eu-cdbr-west-02.cleardb.net',
