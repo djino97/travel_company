1705d502bc771faf10849d664b5523ba6f23801d
djino97 <38541098+djino97@users.noreply.github.com>
2019-05-22 13:35:50 +0300

    Update urls.py

--- kursachback/kursachback/urls.py
+++ kursachback/kursachback/urls.py
@@ -29,3 +29,7 @@ urlpatterns = [
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
+     urlpatterns += patterns('',
+        (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+    )
